a81c6c1689da2d2cddb704082e055655b005c958
RL <lojekrafal27@gmail.com>
2025-11-26 14:32:17 +0100

    Lekcja1

---
+++ Lekcja1/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Lekcja 1");
+
+        Scanner sc = new Scanner(System.in);
+
+        String imie;
+        String nazwisko;
+        int wiek;
+
+//        System.out.print("Podaj imię: ");
+//        imie = sc.next();
+//
+//        System.out.print("Podaj nazwisko: ");
+//        nazwisko = sc.next();
+//
+//        System.out.print("Podaj wiek: ");
+//        wiek = Integer.parseInt(sc.next());
+//
+//        System.out.println(imie + ' ' + nazwisko + "\n" +
+//                "Wiek: " + wiek);
+
+
+        int x = 34;
+        double y = 34.5;
+
+        System.out.println((double)x);
+        System.out.println((int) y);
+
+        System.out.println((char)x);
+
+        //TWORZENIE PLIKU .JAR
+        //1. File -> Project Structure
+        //2. Artifacts (lewe menu)
+        //3. "+"
+        //4. JAR
+        //5. "OK"
+    }
+}                                                                    
